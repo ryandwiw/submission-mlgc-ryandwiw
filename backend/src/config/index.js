7af9515c3d22d6b444009a59b1569d96124e0ecf
Ryan Dwi W <136713247+ryandwiw@users.noreply.github.com>
2024-11-24 22:15:52 +0700

    Update index.js

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  port: process.env.PORT ?? 3000,
+  port: process.env.PORT ?? 8080,
   modelUrl: process.env.MODEL_URL,
   projectId: process.env.PROJECT_ID,
 };
